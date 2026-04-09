22e5455274a9d79c666216802b3b529153b13bfb
fidel-otieno2 <fidelmartins05@gmail.com>
2026-04-10 00:02:34 +0300

    feat: connect mobile app to live backend

--- mobile/src/api/axios.js
+++ mobile/src/api/axios.js
@@ -1,6 +1,9 @@
+import axios from 'axios';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
+const API_URL = 'https://kinscribe-1.onrender.com/api';
 
-const api
+const api = axios.create({ baseURL: API_URL });
 
 api.interceptors.request.use(async (config) => {
   const token = await AsyncStorage.getItem('access_token');
